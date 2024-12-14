2609d13bc4482d4a565034dd0e91e1172b328c60
Alerik Muller <344325@via.dk>
2024-12-14 16:52:31 +0100

    Improvement for css

--- src/test/java/com/via/greeninstalators/GreenInstalatorsApplicationTests.java
+++ src/test/java/com/via/greeninstalators/GreenInstalatorsApplicationTests.java
@@ -1,7 +1,24 @@
 package com.via.greeninstalators;
 
+import com.via.greeninstalators.model.CompanyInfo;
+import com.via.greeninstalators.model.CompanyInstallation;
+import com.via.greeninstalators.model.user.Company;
+import com.via.greeninstalators.repository.CompanyInfoRepository;
+import com.via.greeninstalators.repository.CompanyInstallationRepository;
+import com.via.greeninstalators.repository.CompanyRepository;
+import com.via.greeninstalators.service.AuthenticationService;
+import com.via.greeninstalators.service.CompanyService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class GreenInstalatorsApplicationTests {
